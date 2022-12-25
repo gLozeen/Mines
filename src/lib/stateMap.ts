b25d97cddc09342a5b17c48c9575b9018f72b9fc
Pavel Remarchuk <121202185+gLozeen@users.noreply.github.com>
2022-12-25 15:56:31 +0100

    Innitial commit

--- src/lib/stateMap.ts
+++ src/lib/stateMap.ts
@@ -9,64 +9,78 @@ import {
 } from "./stateMap.types";
 
 export const stateTransitions: GameStateTransitions = {
-  [GameState.Bet]: () => GameState.Setup,
-  [GameState.Setup]: () => GameState.PlayerTurn,
-  [GameState.PlayerTurn]: (payload) => {
-    if (payload?.buttonType === ButtonType.Hit) {
-      return GameState.Hit;
-    } else {
-      return GameState.Stand;
-    }
+  [GameState.Bet]: function (
+    payload?: { betAmount: number } | undefined
+  ): GameState {
+    throw new Error("Function not implemented.");
   },
-  [GameState.EndGame]: () => GameState.Bet,
-  [GameState.Hit]: () =>
-    gameStore.countScore(gameStore.playerHand) < 21
-      ? GameState.PlayerTurn
-      : GameState.EndGame,
-  [GameState.Stand]: () => GameState.DealerTurn,
-  [GameState.DealerTurn]: () => GameState.EndGame,
+  [GameState.Setup]: function (
+    payload?: Record<string, any> | undefined
+  ): GameState {
+    throw new Error("Function not implemented.");
+  },
+  [GameState.Hit]: function (
+    payload?: Record<string, any> | undefined
+  ): GameState {
+    throw new Error("Function not implemented.");
+  },
+  [GameState.Stand]: function (
+    payload?: Record<string, any> | undefined
+  ): GameState {
+    throw new Error("Function not implemented.");
+  },
+  [GameState.PlayerTurn]: function (
+    payload?: { buttonType: ButtonType } | undefined
+  ): GameState {
+    throw new Error("Function not implemented.");
+  },
+  [GameState.DealerTurn]: function (
+    payload?: Record<string, any> | undefined
+  ): GameState {
+    throw new Error("Function not implemented.");
+  },
+  [GameState.EndGame]: function (
+    payload?: Record<string, any> | undefined
+  ): GameState {
+    throw new Error("Function not implemented.");
+  },
 };
 
 export const effectsMap: GameStateEffect[] = [];
 
 export const stateHandlers: GameStateHandlers = {
-  [GameState.Bet]: () => {},
-  [GameState.Setup]: () => {
-    gameStore.clearHands();
-    gameStore.fillDeck();
-    gameStore.dealToDealer();
-    gameStore.dealToDealer();
-    gameStore.dealToPlayer();
-    gameStore.dealToPlayer();
-    gameStore.changeState();
+  [GameState.Bet]: function (
+    payload?: { betAmount: number } | undefined
+  ): void {
+    throw new Error("Function not implemented.");
   },
-  [GameState.PlayerTurn]: () => {},
-  [GameState.EndGame]: () => {
-    gameStore.gameEnd(gameStore.dealerHand, gameStore.playerHand);
-    gameStore.changeState();
+  [GameState.Setup]: function (
+    payload?: Record<string, any> | undefined
+  ): void {
+    throw new Error("Function not implemented.");
   },
-  [GameState.Hit]: () => {
-    gameStore.dealToPlayer();
-    gameStore.changeState();
+  [GameState.Hit]: function (payload?: Record<string, any> | undefined): void {
+    throw new Error("Function not implemented.");
   },
-
-  [GameState.Stand]: () => {
-    gameStore.changeState();
+  [GameState.Stand]: function (
+    payload?: Record<string, any> | undefined
+  ): void {
+    throw new Error("Function not implemented.");
   },
-
-  [GameState.DealerTurn]: () => {
-    if (gameStore.shouldTakeCard()) {
-      setTimeout(() => {
-        gameStore.dealToDealer();
-      }, 500);
-      setTimeout(() => {
-        gameStore.changeState();
-      }, 1000);
-    } else {
-      setTimeout(() => {
-        gameStore.changeState();
-      }, 1000);
-    }
+  [GameState.PlayerTurn]: function (
+    payload?: { buttonType: ButtonType } | undefined
+  ): void {
+    throw new Error("Function not implemented.");
+  },
+  [GameState.DealerTurn]: function (
+    payload?: Record<string, any> | undefined
+  ): void {
+    throw new Error("Function not implemented.");
+  },
+  [GameState.EndGame]: function (
+    payload?: Record<string, any> | undefined
+  ): void {
+    throw new Error("Function not implemented.");
   },
 };
 export { GameState };
